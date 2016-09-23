b0639b4bd45f962c4b8eb9e77db4343ecb5c4077
JW043373 <jwhipple07@gmail.com>
2016-09-23 16:57:32 -0500

    test

--- app/src/main/java/com/truruler/truruler/MainActivity.java
+++ app/src/main/java/com/truruler/truruler/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity
 
     private GestureDetector mGestureDetector;
     private float inchPixelMark, distancePixelMark;
-    ImageView ourView;
+    public ImageView ourView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
